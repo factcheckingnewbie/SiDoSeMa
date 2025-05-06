d238b20b89246ca0c5b6499eb7a5a9628fe4fcce
ptr <200551791+factcheckingnewbie@users.noreply.github.com>
2025-05-06 14:50:56 +0200

    modified:   docker_konsole_persistence/Dockerfile 	modified:   docker_konsole_persistence/runcontainer.sh

--- docker_konsole_persistence/runcontainer.sh
+++ docker_konsole_persistence/runcontainer.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+
 docker run -d \
   --rm \
   --name mykonsole \
+  --cap-add=CHECKPOINT_RESTORE \
+  --cap-add=SYS_ADMIN \
+  --privileged \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   konsole-criu
+
+# docker run -d \
+#   --rm \
+#   --name mykonsole \
+#   -e DISPLAY=$DISPLAY \
+#   -v /tmp/.X11-unix:/tmp/.X11-unix \
+#   konsole-criu
